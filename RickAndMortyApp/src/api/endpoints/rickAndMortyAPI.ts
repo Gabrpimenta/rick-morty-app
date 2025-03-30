e47996c9c417c85b122c8d5c9f7b34762e572e32
Gabriel Pimenta <gabriel.pimenta@mblabs.com.br>
2025-03-30 16:46:35 -0300

    add: create axios API client with request and response interceptors

---
+++ RickAndMortyApp/src/api/endpoints/rickAndMortyAPI.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const RICK_AND_MORTY_API_BASE_URL = 'https://rickandmortyapi.com/api';
+
+const apiClient = axios.create({
+  baseURL: RICK_AND_MORTY_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+apiClient.interceptors.request.use((request) => {
+  console.log('Starting Request', request);
+  return request;
+});
+
+apiClient.interceptors.response.use((response) => {
+  console.log('Received Response', response);
+  return response;
+});
+
+export default apiClient;
