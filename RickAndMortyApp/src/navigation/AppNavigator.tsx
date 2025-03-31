b77543fbebb216030b1014e71f39d99be1f4d63d
Gabriel Pimenta <gabriel.pimenta@mblabs.com.br>
2025-03-30 23:10:44 -0300

    refactor: enhance AppNavigator with theming and clean up screen options

--- RickAndMortyApp/src/navigation/AppNavigator.tsx
+++ RickAndMortyApp/src/navigation/AppNavigator.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 import { CharacterListScreen } from '@/features/characters/screens/CharacterListScreen';
 import { EpisodeListScreen } from '@/features/episodes/screens/EpisodeListScreen';
-import FavoritesScreen from '@/features/favorites/screens/FavoritesScreen';
-import SettingsScreen from '@/features/settings/screens/SettingsScreen';
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { FavoritesScreen } from '@/features/favorites/screens/FavoritesScreen';
+
+import { AppHeader } from '@/components/layout/AppHeader';
 
 import type { TabParamList } from './types';
+import { useTheme } from 'styled-components';
 
 const Tab = createBottomTabNavigator<TabParamList>();
 
 export function AppNavigator() {
+  const theme = useTheme();
   return (
     <Tab.Navigator
-      initialRouteName="CharacterList"
       id={undefined}
       screenOptions={({ route }) => ({
-        headerShown: true,
+        header: (props) => <AppHeader />,
+
         tabBarIcon: ({ focused, color, size }) => {
           let iconName: keyof typeof Ionicons.glyphMap = 'alert-circle-outline';
           if (route.name === 'CharacterList') {
@@ -31,20 +34,14 @@ export function AppNavigator() {
           }
           return <Ionicons name={iconName} size={size} color={color} />;
         },
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.textSecondary,
+        tabBarStyle: { backgroundColor: theme.colors.surface, borderTopColor: theme.colors.border },
       })}
     >
-      <Tab.Screen
-        name="CharacterList"
-        component={CharacterListScreen}
-        options={{ title: 'Characters' }}
-      />
-      <Tab.Screen
-        name="EpisodeList"
-        component={EpisodeListScreen}
-        options={{ title: 'Episodes' }}
-      />
-      <Tab.Screen name="Favorites" component={FavoritesScreen} options={{ title: 'Favorites' }} />
-      <Tab.Screen name="Settings" component={SettingsScreen} options={{ title: 'Settings' }} />
+      <Tab.Screen name="CharacterList" component={CharacterListScreen} />
+      <Tab.Screen name="EpisodeList" component={EpisodeListScreen} />
+      <Tab.Screen name="Favorites" component={FavoritesScreen} />
     </Tab.Navigator>
   );
 }
