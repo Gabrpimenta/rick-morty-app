cf4a97a3b875313e45b6d653bc64ce493b5bb9ba
Gabriel Pimenta <gabriel.pimenta@mblabs.com.br>
2025-03-30 23:13:37 -0300

    add: create styles for FavoritesScreen layout and padding

---
+++ RickAndMortyApp/src/features/favorites/screens/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const LIST_PADDING = 16;
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  listContentContainer: {
+    paddingHorizontal: LIST_PADDING,
+    paddingBottom: LIST_PADDING,
+  },
+});
